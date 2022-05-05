607c062d10625163c9077b9d3a6313dbba7c129b
Little_Leier <917815710@qq.com>
2022-05-05 09:44:41 +0800

    UPDATE
    update

---
+++ Project2/Random/app/src/main/java/com/example/random/SecondFragment.kt
@@ -0,0 +1,57 @@
+package com.example.random
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import com.example.random.databinding.FragmentSecondBinding
+import androidx.navigation.fragment.navArgs
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    val args: SecondFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val count = args.myArgs
+        view.findViewById<TextView>(R.id.textview_header).text = count.toString()
+
+        val random = java.util.Random()
+        var randomNumber = 0
+        randomNumber = random.nextInt()
+
+        view.findViewById<TextView>(R.id.textview_random).text = randomNumber.toString()
+
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
